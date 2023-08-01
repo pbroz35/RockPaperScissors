57a5f4f101eed691399ffe54814655d28059041a
pbroz35 <1brozek.peter@gmail.com>
2023-08-01 16:28:58 -0500

    fixed playerChoice issue where variable was not being sent

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
-let playerAnswer;
+//Initialize variables
+let computerAnswer;
 
-
-
-//randomly returns 'Rock' 'paper' or 'scissors' on behalf of computer
+//Generate computer answer
 function getComputerChoice()
 {
 //Generate random choice
@@ -11,28 +10,26 @@ let random=Math.floor(Math.random() * 3);
 //Decide which choice based off random number generated
 if(random==0)
 {
-    console.log('Rock!');
+    return computerAnswer='rock';
 }
 else if(random==1)
 {
-    console.log('Paper!');
+    return computerAnswer='paper';
 }
 else if(random==2)
 {
-    console.log('Scissors!');
-}
-else
-{
-    console.log('Error generating computer choice.');
-}
+    return computerAnswer='scissors';
 }
 
+}
+
+//Have player choose an option 
 function playerSelection()
 {
-    let validAnswer=0;
+    // let validAnswer=0;
 
-    while(validAnswer==0) //while-loop to force user to enter valid input
-    {
+    // while(validAnswer==0) //while-loop to force user to enter valid input
+    // {
     let playerAnswer=prompt('Rock, paper, scissors?');
     playerAnswer=playerAnswer.toLowerCase();
 
@@ -41,31 +38,47 @@ function playerSelection()
     if (playerAnswer== 'rock' | playerAnswer=='paper' | playerAnswer=='scissors') //Check if player entered valid answer.
     {
         console.log(`You've selected ${playerAnswer}!`);
-        validAnswer=1;
-    }
-    else{ //prompt user to enter a valid answer.
-        console.log(`You've selected an incorrect input : ${playerAnswer} \nEnter a new answer!`);
-    }
+        return playerAnswer;
 
     }
- 
-return playerAnswer;
+
+}
+
+function playRound(playerSelection, computerSelection) 
+{
+    if(playerSelection==computerSelection)
+    {
+        alert('tie!');
+    }    
+
+}
+
+let playerChoice=playerSelection();
+let computerChoice=getComputerChoice();
+
+playRound(playerChoice,computerChoice);
+
+playRound(playerChoice,computerChoice)
+{
+
+    if(playerChoice=computerChoice)
+    {
+        console.log('tie!');
+    }
 
 
 
-//sreturn playerAnswer;
-
-// return playerAnswer;
 }
 
 
-/*
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
    
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+// let rounds=prompt('How many rounds you want to play?');
 
-  */
+// for (let i = 0; i <= rounds; i++) 
+// {
+
+    
+// }
+
+
+
